f311b13243ad38f5da5a5aa5347e5ddb615e666f
Atanas <atanasdraganov77@gmail.com>
2022-12-03 22:05:58 +0200

    fire emoji added next to promotion prices

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let hotEls = document.querySelectorAll(".hot");
+  for(let i=0; i<hotEls.length; i++){
+    hotEls[i].innerHTML += " 🔥";
+  }
 });
